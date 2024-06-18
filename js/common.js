55d9d978689d467cef7abc8544a57327977142bb
jangtjdgns <jangtjdgns0208@gmail.com>
2024-06-19 07:23:15 +0900

    테스트 커밋

---
+++ js/common.js
@@ -0,0 +1,38 @@
+// 공용 js
+
+// 섹션별 애니메이션 효과
+function runAnimation(index) {
+    // 메인 섹션 애니메이션 효과
+    if(index === 0) {
+        setTimeout(function () {
+            $('.title-border').removeClass('-translate-x-full');
+        }, 500)
+
+        setTimeout(function () {
+            $('.title-name').removeClass('opacity-0');
+            $('.title-border')
+                .removeClass('h-2 duration-[2500ms]')
+                .addClass('h-16 duration-[900ms]');
+        }, 3000)
+
+        setTimeout(function () {
+            $('.moew-icon, .mouse-icon').removeClass('opacity-0');
+            $('.title-box').addClass('neon-box-purple');
+        }, 4000)
+    }
+
+    // 프로필 섹션 애니메이션 효과
+    else if(index === 1) {
+        $('.pf-profile .pf-profile-wrap').addClass('profile-animation');
+        $('.pf-profile-wrap>.spin-wrap>span').addClass('spin');
+        $('.pf-profile .pf-profile-title').addClass('text-shadow-neon');
+        $('.pf-profile .pf-profile-content-wrap>.pf-profile-content-image').removeClass('opacity-0')
+        $('.pf-profile .pf-profile-content-wrap>.pf-profile-content-text').removeClass('opacity-0')
+    }
+
+    // 스킬 섹션 애니메이션 효과
+    else if(index === 2) {
+        $('.pf-skills .pf-skills-title').addClass('text-shadow-neon');
+        initializeRadialProgress();
+    }
+}
